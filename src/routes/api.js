e96bb524c027dfb12dedd1395637b0a1aa7ea26d
Vinnie Magro <vmagro@socaldevs.com>
2015-09-05 03:44:03 -0400

    Modify artifact endpoint to allow version other than latest

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,12 @@ var request = require('superagent');
 
 var jenkins = require('../jenkins');
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-	app.get('/api/:app/latest/artifact', function(req, res) {
-		jenkins.last_build_info(req.params.app, function(err, data) {
+	app.get('/api/:app/:version/artifact', function(req, res) {
+		var app = req.params.app;
+		var version = req.params.version;
+		var callback = function(err, data) {
 			if (err) {
 				res.status(500);
 				res.send(err);
@@ -13,10 +15,16 @@ module.exports = function (app) {
 				var artifactUrl = data.url + 'artifact/' + data.artifacts[0].relativePath;
 				request.get(artifactUrl).pipe(res);
 			}
-		});
+		};
+
+		if (version === 'latest') {
+			jenkins.last_build_info(app, callback);
+		} else {
+			jenkins.build_info(app, version, callback);
+		}
 	});
 
-	app.get('/api/projects', function (req, res) {
+	app.get('/api/projects', function(req, res) {
 		jenkins.all_jobs(function(err, data) {
 			res.send(data);
 		});
