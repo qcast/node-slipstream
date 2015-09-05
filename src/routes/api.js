b4741aaf776a3b2ec26a08f7d84facd4301862ce
Vinnie Magro <vmagro@socaldevs.com>
2015-09-04 23:53:37 -0400

    Begin jenkins integration

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,27 @@
+var jenkinsapi = require('jenkins-api');
+var jenkins = jenkinsapi.init('http://192.168.99.100:8080');
+
 module.exports = function (app) {
 
 	app.get('/api/:app/latest', function(req, res) {
-		res.send({
-			app: req.params.app
+		jenkins.last_build_info(req.params.app, function(err, data) {
+			if (err) {
+				res.status(500);
+				res.send(err);
+			} else {
+				var response = {
+					number: data.number,
+					artifact: data.url + 'artifact/' + data.artifacts[0].fileName
+				};
+
+				res.send(response);
+			}
+		});
+	});
+
+	app.get('/api/projects', function (req, res) {
+		jenkins.all_jobs(function(err, data) {
+			res.send(data);
 		});
 	});
 
