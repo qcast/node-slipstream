70fe737a44b412fe7a38d37d928943cc8f608a08
Vinnie Magro <vmagro@socaldevs.com>
2015-09-05 14:02:13 -0400

    Improved project endpoints

--- src/jenkins.js
+++ src/jenkins.js
@@ -1,4 +1,20 @@
+var Q = require('q');
+
 var jenkinsapi = require('jenkins-api');
 var jenkins = jenkinsapi.init(process.env.JENKINS_PORT_8080_TCP.replace('tcp', 'http'));
+
+jenkins.job = function (job) {
+	var deferred = Q.defer();
+	jenkins.job_info(job, function(err, job) {
+		if (err) {
+			deferred.reject(err);
+			return;
+		}
+		job.metadata = JSON.parse(job.description);
+		console.log(job);
+		deferred.resolve(job);
+	});
+	return deferred.promise;
+};
 
 module.exports = jenkins;
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,5 @@
 var request = require('superagent');
+var async = require('async');
 
 var jenkins = require('../jenkins');
 
@@ -24,9 +25,50 @@ module.exports = function(app) {
 		}
 	});
 
+	app.get('/api/:app/:version', function (req, res) {
+		var app = req.params.app;
+		var version = req.params.version;
+		var callback = function(err, data) {
+			if (err) {
+				res.status(500);
+				res.send(err);
+			} else {
+				res.send(data);
+			}
+		};
+
+		if (version === 'latest') {
+			jenkins.last_build_info(app, callback);
+		} else {
+			jenkins.build_info(app, version, callback);
+		}
+	});
+
+	app.get('/api/:app', function (req, res) {
+		var app = req.params.app;
+		jenkins.job(app).then(function(job) {
+			res.send(job);
+		}).fail(function(err) {
+			res.status(500);
+			res.send(err);
+		});
+	});
+
 	app.get('/api/projects', function(req, res) {
 		jenkins.all_jobs(function(err, data) {
-			res.send(data);
+			async.map(data, function (job, cb) {
+				jenkins.job_info(job.name, function (err, job) {
+					console.log(job.description);
+					cb(err, job);
+				});
+			}, function (err, projects) {
+				async.filter(projects, function (project) {
+					return project.description === 'slipstream';
+				}, function (projects) {
+					res.send(projects);
+				});
+				res.send(projects);
+			});
 		});
 	});
 
